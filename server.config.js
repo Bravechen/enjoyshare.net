61c2696be6f5de56321723fedc3c160540375f5e
Bravechen <brave_cgx@163.com>
2019-11-23 06:10:13 +0800

    修改server.config.js

--- server.config.js
+++ server.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const hbsSettings = require('./plugins/hbsSetting');
-
+const projectRoot = process.cwd();
 const serverConfig = {
   port: process.env.PORT || '3000',
-  viewsDir: path.join(process.cwd(), './web/views'),
-  staticDirs: [{ route: '/static', dir: path.join(process.cwd(), './web/static') }],
+  viewsDir: path.join(projectRoot, './web/views'),
+  staticDirs: [{ route: '/static', dir: path.join(projectRoot, './web/static') }],
   viewEngine: {
     engine: 'hbs',
     defaultLayout: './layouts/layout',
     partials: [
-      path.join(process.cwd(), './web/views/components'), 
-      path.join(process.cwd(), './web/views/templates')
+      path.join(projectRoot, './web/views/components'), 
+      path.join(projectRoot, './web/views/templates')
     ],
     settings: [hbsSettings]
   }
